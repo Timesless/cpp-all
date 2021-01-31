a68b2bd2f39f0a605bb8c25f4dfc26c473941ed3
yangzl <yangzl@git.com>
2021-01-31 14:47:00 +0800

    'sync'

---
+++ itcast/oop/callback_func.cpp
@@ -0,0 +1,32 @@
+/**
+ * @author yangzl
+ * @date 2021/1/31
+ * @desc
+ *
+ *      1. 数组与指针
+ *      2. 函数与指针
+ *          回调函数
+ */
+
+#include <iostream>
+
+using namespace std;
+
+
+void test_array_pointer() {
+    int arr[10] = new int[10];
+
+}
+
+/**
+ *
+ *
+ * @param argc
+ * @param argv
+ * @return
+ */
+
+int main(int argc, char *const argv[]) {
+
+    return 0;
+}
